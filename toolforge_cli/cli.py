f52108160942f80fccdca9644eb04dd4bacb8772
David Caro <dcaro@wikimedia.org>
2022-03-24 18:37:04 +0100

    Add build-show command
    Bug: T293644
    Change-Id: I8f3aec0523f5a7845f1216c3e42d867244010e77
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- toolforge_cli/cli.py
+++ toolforge_cli/cli.py
@@ -6,7 +6,7 @@ import subprocess
 import sys
 import time
 from pathlib import Path
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, List, Tuple
 
 import click
 
@@ -39,6 +39,80 @@ def _run_to_short_str(run: Dict[str, Any]) -> str:
     builder_image = next(param for param in run["spec"]["params"] if param["name"] == "BUILDER_IMAGE")["value"]
     source_url = next(param for param in run["spec"]["params"] if param["name"] == "SOURCE_URL")["value"]
     return f"{run_name}\t{status}\t{start_time}\t{end_time}\t{source_url}\t{repo_url}\t{image_name}\t{image_tag}\t{builder_image}"
+
+
+def _get_status_data_lines(k8s_obj: Dict[str, Any]) -> List[str]:
+    status_data_lines = []
+    status_data = next(condition for condition in k8s_obj["status"]["conditions"] if condition["type"] == "Succeeded")
+    start_time = k8s_obj["status"]["startTime"]
+    status_data_lines.append(f"{click.style('Start time:', bold=True)} {start_time}")
+    end_time = k8s_obj["status"].get("completionTime", click.style("running", fg="green"))
+    status_data_lines.append(f"{click.style('End time:', bold=True)} {end_time}")
+
+    if status_data["status"] == "True" or status_data["reason"] == "Running":
+        status_color = "green"
+        status_name = "ok"
+    elif status_data["status"] == "False":
+        status_color = "red"
+        status_name = "error"
+    else:
+        status_color = "yellow"
+        status_name = status_data["status"].lower()
+
+    status = f"{click.style(status_name, fg=status_color)}({status_data['reason']})"
+    status_data_lines.append(f"{click.style('Status:', bold=True)} {status}")
+    status_data_lines.append(f"{click.style('Message:', bold=True)} {status_data['message']}")
+
+    return status_data_lines
+
+
+def _get_step_details_lines(task: Dict[str, Any]) -> List[str]:
+    steps_details_lines = []
+    for step in task["status"]["steps"]:
+        step_status = (
+            click.style("ok", fg="green") if step["terminated"]["exitCode"] == 0 else click.style("error", fg="red")
+        )
+        steps_details_lines.append(
+            f"{click.style('Step:', bold=True)} {step['name']} - {step_status}({step['terminated']['reason']})"
+        )
+
+    return steps_details_lines
+
+
+def _get_task_details_lines(run: Dict[str, Any]) -> List[str]:
+    tasks_details_lines = []
+    for task in run["status"]["taskRuns"].values():
+        tasks_details_lines.append(f"{click.style('Task:', bold=True)} {task['pipelineTaskName']}")
+        tasks_details_lines.extend("    " + line for line in _get_status_data_lines(k8s_obj=task))
+        tasks_details_lines.append(click.style("    Steps:", bold=True))
+        tasks_details_lines.extend("        " + line for line in _get_step_details_lines(task=task))
+        tasks_details_lines.append("")
+
+    return tasks_details_lines
+
+
+def _run_to_details_str(run: Dict[str, Any]) -> str:
+    details_str = ""
+    run_name = run["metadata"]["name"]
+    details_str += f"{click.style('Name:', bold=True)} {click.style(run_name, fg='blue')}\n"
+
+    details_str += "\n".join(_get_status_data_lines(k8s_obj=run)) + "\n"
+
+    app_image = next(param for param in run["spec"]["params"] if param["name"] == "APP_IMAGE")["value"]
+    repo_url, image_name, image_tag = _app_image_to_parts(app_image)
+    builder_image = next(param for param in run["spec"]["params"] if param["name"] == "BUILDER_IMAGE")["value"]
+    source_url = next(param for param in run["spec"]["params"] if param["name"] == "SOURCE_URL")["value"]
+    details_str += click.style("Parameters:\n", bold=True)
+    details_str += f"    {click.style('source_url:', bold=True)} {source_url}\n"
+    details_str += f"    {click.style('image_name:', bold=True)} {image_name}\n"
+    details_str += f"    {click.style('image_tag:', bold=True)} {image_tag}\n"
+    details_str += f"    {click.style('repo_url:', bold=True)} {repo_url}\n"
+    details_str += f"    {click.style('builder_image:', bold=True)} {builder_image}\n"
+
+    details_str += click.style("Tasks:\n", bold=True)
+    details_str += "\n".join("    " + line for line in _get_task_details_lines(run=run))
+
+    return details_str
 
 
 def _app_image_to_parts(app_image: str) -> Tuple[str, str, str]:
@@ -201,6 +275,47 @@ def build_list(kubeconfig: Path, json: bool) -> None:
                     indent=4,
                 )
             )
+
+
+@toolforge.command(name="build-show")
+@click.argument("RUN_NAME")
+@click.option(
+    "--kubeconfig",
+    help="Path to the kubeconfig file.",
+    default=Path(os.environ.get("KUBECONFIG", "~/.kube/config")),
+    type=Path,
+    show_default=True,
+)
+@click.option(
+    "--json",
+    help="If set, will output in json format.",
+    is_flag=True,
+)
+def build_show(run_name: str, kubeconfig: Path, json: bool) -> None:
+    k8s_client = K8sAPIClient.from_file(kubeconfig=kubeconfig, namespace=TBS_NAMESPACE)
+    run = k8s_client.get_object(kind="pipelineruns", name=run_name)
+    app_image = next(param for param in run["spec"]["params"] if param["name"] == "APP_IMAGE")["value"]
+    repo_url, image_name, image_tag = _app_image_to_parts(app_image=app_image)
+    status = next(condition for condition in run["status"]["conditions"] if condition["type"] == "Succeeded")
+    if not json:
+        click.echo(_run_to_details_str(run=run))
+    else:
+        json_mod.dumps(
+            {
+                "name": run["metadata"]["name"],
+                "params": {
+                    "image_name": image_name,
+                    "image_tag": image_tag,
+                    "repo_url": repo_url,
+                },
+                "status": {
+                    "succeeded": status["Status"],
+                    "message": status["Message"],
+                    "reason": status["Reason"],
+                },
+            },
+            indent=4,
+        )
 
 
 def main():
